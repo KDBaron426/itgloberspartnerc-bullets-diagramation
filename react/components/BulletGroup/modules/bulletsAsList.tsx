157a69b220975e8209998e35abb53567151f4515
Karen Baron <karen.baron@vela1.com>
2022-11-23 15:50:33 -0500

    refactor__css of the bullet container

---
+++ react/components/BulletGroup/modules/bulletsAsList.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { BulletsSchema } from '../BulletTypes'
+import Bullet from '../Bullet'
+
+export const getBulletsAsTSXList = (bullets: BulletsSchema) =>
+  bullets.map((bullet: any, index) => {
+    console.log(bullet)
+
+    return (
+      <Bullet
+        key={index}
+        src={bullet.image}
+        titleBullet={bullet.titleBullet}
+        link={
+          bullet.link
+            ? bullet.link
+            : {
+                url: '/',
+                attributeNoFollow: false,
+                attributeTitle: '',
+                openNewTab: false,
+                newTab: false,
+              }
+        }
+      />
+    )
+  })
